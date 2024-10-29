b62411e9e0ea01c314384e8f593b2070f73c1840
Anton Ha <antonha016@gmail.com>
2024-10-29 17:36:11 -0400

    got netlify deployment to work!

--- client/src/supabaseClient.ts
+++ client/src/supabaseClient.ts
@@ -4,8 +4,6 @@ import { createClient } from '@supabase/supabase-js';
 // Access environment variables with `import.meta.env`
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL.trim();
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY.trim();
-console.log("Supabase URL:", supabaseUrl);
-console.log("Supabase Anon Key:", supabaseAnonKey);
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Supabase environment variables are missing or invalid.");
